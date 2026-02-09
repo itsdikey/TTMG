53e53a2c9cce110d752d41ea459e9e2b1cd075ea
Garegin Tadevosyan <garo.tadevosyan@gmail.com>
2026-02-09 15:21:43 +0000

    fix: less-privelege (#12)
    * refactor
    
    * feat: autocomplete
    
    * feat: added initial secret service
    
    * feat: improved usability of secrete service only requiring script once
    
    * fix: fixed yaml for version deployment
    
    * fix: less privelege
    
    * fix: less privelege

--- TTMG/Interfaces/IConfigService.cs
+++ TTMG/Interfaces/IConfigService.cs
@@ -3,6 +3,7 @@ namespace TTMG.Interfaces
     public interface IConfigService
     {
         AppConfig Config { get; }
+        string DataDirectory { get; }
         void LoadConfig();
         void SaveConfig();
         void EnsureDefaultConfig();
